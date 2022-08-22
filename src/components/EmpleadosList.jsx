285896ae3c99c56d56015ce8ae33afdbb8682cf1
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-22 15:50:30 -0300

    Acomodado archivos para el projecto

---
+++ src/components/EmpleadosList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmpleadosList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EmpleadosList;
---
+++ src/components/EmpleadosRow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmpleadosRow = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EmpleadosRow;
